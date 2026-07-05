cc906bf0b736c0df9e888d5e158ab3631ae6333e
Nataliya Krylova <nataliya.krylova2022@gmail.com>
2026-07-05 11:29:09 +0200

    feat: create swagger editor

---
+++ components/SwaggerEditor/SwaggerEditor.tsx
@@ -0,0 +1,121 @@
+'use client';
+
+import Editor from '@monaco-editor/react';
+import * as yaml from 'js-yaml';
+import { useState, useEffect } from 'react';
+import type { Format, SwaggerSchema } from '@/types/swagger';
+import parseSchema from '@/lib/parseSchema';
+import detectFormat from '@/lib/detectFormat';
+import validateSchema from '@/lib/validateSchema';
+import Toolbar from './Toolbar';
+import ValidationMessage from './ValidationMessage';
+
+const VALIDATION_DELAY = 1000;
+
+type SwaggerEditorProps = {
+  schema: SwaggerSchema | null;
+  onSchemaChange: (schema: SwaggerSchema | null) => void;
+};
+
+export default function SwaggerEditor({
+  schema,
+  onSchemaChange,
+}: SwaggerEditorProps) {
+  const [editorValue, setEditorValue] = useState(
+    `
+      openapi: 3.0.0
+      info:
+        title: My API
+        version: 1.0.0
+      paths:
+        /pets:
+          get:
+            summary: Get pets
+            responses:
+              "200":
+                description: OK
+          post:
+            summary: Create pet
+            responses:
+              "201":
+                description: Created
+
+        /users:
+          get:
+            summary: Get users
+            responses:
+              "201":
+                description: OK
+    `,
+  );
+  const [format, setFormat] = useState<Format>('yaml');
+  const [validationError, setValidationError] = useState<string | null>(null);
+
+  function handleChange(value: string | undefined) {
+    const text = value ?? '';
+    setEditorValue(text);
+  }
+
+  function switchToJSON() {
+    if (!schema) return;
+    setEditorValue(JSON.stringify(schema, null, 2));
+    setFormat('json');
+  }
+
+  function switchToYAML() {
+    if (!schema) return;
+    setEditorValue(yaml.dump(schema));
+    setFormat('yaml');
+  }
+
+  function handleSave() {
+    console.log('Save schema', schema);
+  }
+
+  useEffect(() => {
+    const timer = setTimeout(async () => {
+      console.log('Validating');
+      const format = detectFormat(editorValue);
+      setFormat(format);
+
+      try {
+        const parsed = parseSchema(editorValue, format);
+
+        await validateSchema(parsed as SwaggerSchema);
+        onSchemaChange(parsed as SwaggerSchema);
+        setValidationError(null);
+
+        console.log(parsed);
+        console.log('VALID');
+      } catch (e) {
+        onSchemaChange(null);
+        if (e instanceof Error) {
+          setValidationError(e.message);
+          console.log(e.message);
+        }
+      }
+    }, VALIDATION_DELAY);
+
+    return function cleanup() {
+      clearTimeout(timer);
+    };
+  }, [editorValue]);
+
+  return (
+    <div>
+      <Toolbar
+        format={format}
+        onConvertToJSON={switchToJSON}
+        onConvertToYAML={switchToYAML}
+        onSave={handleSave}
+      />
+      <Editor
+        height="200px"
+        language={format}
+        value={editorValue}
+        onChange={handleChange}
+      />
+      {validationError && <ValidationMessage message={validationError} />}
+    </div>
+  );
+}
