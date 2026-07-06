fa0a8ce78b1b4328dc66c43ddd8eb1cd1b75bdc5
Nataliya Krylova <nataliya.krylova2022@gmail.com>
2026-07-06 19:38:51 +0200

    feat: display request body

--- components/SwaggerWorkspace/SwaggerViewer/Endpoint/RequestBody/RequestBody.tsx
+++ components/SwaggerWorkspace/SwaggerViewer/Endpoint/RequestBody/RequestBody.tsx
@@ -6,13 +6,55 @@ export type RequestBodyProps = {
 
 export default function RequestBody({ requestBody }: RequestBodyProps) {
   return (
-    <section className="px-4 py-2 flex gap-4">
+    <section className="px-4 py-2 flex flex-col gap-4">
       <h3 className="font-semibold">Request Body:</h3>
-      {Object.entries(requestBody.content ?? {}).map(([contentType, media]) => (
-        <div key={contentType}>
-          <strong>{contentType}</strong>
+      {requestBody.description && (
+        <div className="pl-5">
+          <h4>Description:</h4>
+          <p>{requestBody.description}</p>
         </div>
-      ))}
+      )}
+      {requestBody.required && (
+        <div className="pl-5">
+          <h4>Required:</h4>
+          <p>{requestBody.required ? 'yes' : 'no'}</p>
+        </div>
+      )}
+      {requestBody.content && (
+        <>
+          <div className="pl-5">
+            <h4>Content:</h4>
+            <p>{Object.keys(requestBody.content)[0]}</p>
+          </div>
+          {Object.entries(requestBody.content ?? {}).map(
+            ([contentType, media]) => (
+              <div key={contentType}>
+                <h4>{contentType}</h4>
+                <div className="pl-5 flex flex-col gap-4">
+                  {media.schema && (
+                    <div className="pl-4">
+                      <h5 className="font-medium">Schema</h5>
+
+                      <pre>{JSON.stringify(media.schema, null, 2)}</pre>
+                    </div>
+                  )}
+
+                  {media.example && (
+                    <div className="pl-4">
+                      <h5 className="font-medium">Example</h5>
+
+                      <pre>{JSON.stringify(media.example, null, 2)}</pre>
+                    </div>
+                  )}
+                </div>
+              </div>
+            ),
+          )}
+        </>
+      )}
     </section>
   );
 }
+/**
+ *
+ */
