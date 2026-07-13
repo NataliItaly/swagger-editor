9aeeb754b7369fe7e0e85a94c9c7917d8153c010
Nataliya Krylova <nataliya.krylova2022@gmail.com>
2026-07-13 23:04:02 +0200

    feat: test header component

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -7,9 +7,7 @@ export default function Header() {
   return (
     <header className="flex justify-between gap-6 items-center px-6 py-5 border-b border-gray-400 ">
       <nav className="flex gap-10 items-center">
-        <div className="text-2xl font-bold">
-          <span>Swagger</span>API
-        </div>
+        <div className="text-2xl font-bold">SwaggerAPI</div>
         <ul className="flex items-center gap-6 text-xl">
           <li>
             <Link href="/swagger">Swagger</Link>
