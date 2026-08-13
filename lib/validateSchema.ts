d1ef18f7e4833b9b5f6af43cfead5dd8a31ce695
Nataliya Krylova <nataliya.krylova2022@gmail.com>
2026-08-13 02:30:20 +0200

    fix: schema validation type

--- lib/validateSchema.ts
+++ lib/validateSchema.ts
@@ -2,5 +2,5 @@ import type { SwaggerSchema } from '@/types/swagger';
 import SwaggerParser from '@apidevtools/swagger-parser';
 
 export default async function validateSchema(schema: SwaggerSchema) {
-  return SwaggerParser.validate(schema);
+  return SwaggerParser.validate(JSON.stringify(schema));
 }
